4e2531e9172396568cdc58eeb6f3d2e39680b09e
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-07-30 18:26:06 +0300

    Added stack methods

--- data-structures-stack/CustomStack.cs
+++ data-structures-stack/CustomStack.cs
@@ -4,8 +4,26 @@ using System.Text;
 
 namespace data_structures_stack
 {
-    class CustomStack <T>
+    public class CustomStack <T>
     {
+        private T[] _stack;
+        private int _capacity;
+        private int top;
 
+        public CustomStack(int size)
+        {
+            _stack = new T[size];
+            _capacity = size;
+        }
+
+        public void Push (T item)
+        {
+            
+        }
+
+        public void Pop (T item)
+        {
+
+        }
     }
 }
--- data-structures-stack/Program.cs
+++ data-structures-stack/Program.cs
@@ -7,6 +7,11 @@ namespace data_structures_stack
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var customStack = new CustomStack<int>();
+
+            customStack.Push(1);
+            customStack.Push(2);
         }
     }
 }
